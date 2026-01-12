b3edaefaf5568cdadd00441ef3f294adcb172c6d
eriknegu <u1997102@campus.udg.edu>
2026-01-12 11:40:38 +0100

    Delete constants.h

--- constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-// així no he de declarar-ho a cada classe que ho necessita
-
-inline constexpr int MAX_TARJETES_EQUIP = 3;
-inline constexpr int MAX_EXCLUSIONS_JUGADOR = 2;
-
-#endif
